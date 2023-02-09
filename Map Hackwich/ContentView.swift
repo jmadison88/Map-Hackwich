68c10da8cc148e1d9f78d9dab381748333083e11
jmadison <jmad99@sbcglobal.net>
2023-02-09 09:35:05 -0600

    Added user location

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+@StateObject var locationManager = LocationManager()
+@State private var userTrackingMode: MapUserTrackingMode = .follow
 @State private var region = MKCoordinateRegion(
     center: CLLocationCoordinate2D(
         latitude: 42.15704,
@@ -17,13 +19,10 @@ struct ContentView: View {
         latitudeDelta: 0.05,
         longitudeDelta: 0.05))
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Map(coordinateRegion: $region)
-        }
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode)
     }
 }
 
